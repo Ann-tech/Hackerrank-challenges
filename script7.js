425923c11f702a1ca550fb8cbd1b95cfaa5a9ca6
Ann-tech <58173580+Ann-tech@users.noreply.github.com>
2022-06-03 14:49:48 +0100

    Hackerrank challenge - Mini-max Sum

--- script7.js
+++ script7.js
@@ -1,1 +1,7 @@
-
+//see instructions here - https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
+function miniMaxSum(arr) {
+    // Write your code here
+  let min = [...arr].sort((a, b) => b - a).slice(1).reduce((sum, item) => sum + item, 0);
+  let max = [...arr].sort((a, b) => a - b).slice(1).reduce((sum, item) => sum + item, 0);
+  console.log(`${min} ${max}`);
+}
