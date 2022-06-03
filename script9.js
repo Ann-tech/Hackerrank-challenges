3bedbc9bd2826f6ba97c97beab477d8f4ccbd279
Ann-tech <58173580+Ann-tech@users.noreply.github.com>
2022-06-03 15:22:26 +0100

    Hackerrank challenge - Time conversion

--- script9.js
+++ script9.js
@@ -1,1 +1,12 @@
+//see instructions here - https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
 
+function timeConversion(s) {
+  let splitTime = [s.slice(0, 2), s.slice(3, 5), s.slice(6, 8), s.slice(-2)];
+  
+  if (splitTime[3] == "AM") {
+      return `0${+splitTime[0] % 12}:${splitTime[1]}:${splitTime[2]}`;
+  } else if (splitTime[3] == "PM" && splitTime[0] >= 1 && splitTime[0] < 12) {
+    return `${+splitTime[0] + 12}:${splitTime[1]}:${splitTime[2]}`;
+  }
+  return `${splitTime[0]}:${splitTime[1]}:${splitTime[2]}`
+}
