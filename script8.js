849a60428be17cf2b5d8c9f026d57e9277c9c4bf
Ann-tech <58173580+Ann-tech@users.noreply.github.com>
2022-06-03 14:51:38 +0100

    Hackerrank challenge - Birthday cake candles

--- script8.js
+++ script8.js
@@ -1,1 +1,11 @@
-
+//see instructions here - https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let max = Math.max(...candles);
+    let count = 0;
+    
+    candles.forEach(item => {
+      if (item == max) count++
+    });
+    return count;
+}
